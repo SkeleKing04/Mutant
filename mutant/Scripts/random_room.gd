cbe7c18c9d62675c2db0452aa3445aecae795f83
Jackson <jacksonjlucas04@gmail.com>
2025-09-29 12:23:53 +0930

    Got random object placement working. Changed default scene to a testing scene, complete with lighting and a cam. Made some box scenes for testing the random placement. Random placement can also rotate objects. I'll want to add some variables later to fine tune the position and rotation, along with the ammount of objects.

--- mutant/Scripts/random_room.gd
+++ mutant/Scripts/random_room.gd
@@ -1,8 +1,18 @@
 extends Node3D
 
-@export var AssetPoints : Array[Node]
-
+@export var AssetPoints : Array[Node3D]
+@export var AssetScenes : Array[PackedScene]
 
 func _on_ready() -> void:
-	var index = randi_range(0, AssetPoints.size() - 1)
-	print(AssetPoints[index].name)
+	var hits = 0
+	for point in AssetPoints:
+		if(randi_range(0, 10) > 2):
+			hits += 1
+			var newInst = AssetScenes[randi_range(0, AssetScenes.size() - 1)].instantiate()
+			add_child(newInst)
+			#for child in get_children():
+			#	if child is Node3D and newInst:
+			newInst.position = point.position
+			newInst.rotation = Vector3(0, randi_range(0, 614) / 100.0, 0)
+			print(newInst.name + " was placed at " + point.name)
+	print("in total, " + str(hits) + " objects were placed")
